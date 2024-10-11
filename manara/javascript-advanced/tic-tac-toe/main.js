89c2e335cbb7d66de5d506649850544c63d1bf3d
Mostafa <mostafafinal55@gmail.com>
2024-10-11 18:55:19 +0300

    Manara JS Advanced Tic-Tac-Toe gameboard module

---
+++ manara/javascript-advanced/tic-tac-toe/main.js
@@ -0,0 +1,15 @@
+// gameboard
+const GameBoard = (function () {
+  const row = 3;
+  const column = 3;
+  const board = [];
+
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+
+    for (let j = 0; j < column; j++) {
+      board[i].push("");
+    }
+  }
+  return { board };
+})();
